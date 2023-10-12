603e160dc7dd1b484b4e7278132fac42dc5e0a50
marcslr <sainthilaire.marcjoel@gmail.com>
2023-10-12 13:24:27 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'boostrap/dist/css/boostrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={}> </Route>
+        <Route path='/create' element={}> </Route>
+        <Route path='/update' element={}> </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+     
+  )
+}
+
+export default App
